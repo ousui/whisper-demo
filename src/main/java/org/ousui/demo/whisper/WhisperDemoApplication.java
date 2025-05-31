63e00d4e4354ca1580eecaa90c11456f0b1acec7
SHUAI.W <x@ousui.org>
2025-05-31 22:58:42 +0800

    build: init

---
+++ src/main/java/org/ousui/demo/whisper/WhisperDemoApplication.java
@@ -0,0 +1,13 @@
+package org.ousui.demo.whisper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhisperDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhisperDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ousui/demo/whisper/WhisperDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ousui.demo.whisper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhisperDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
